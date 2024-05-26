97c7a3cb49001f30de8e63b6fdfe63ee57ed341e
Javiex <javiexalonso@gmail.com>
2024-05-27 00:09:56 +0200

    Version 3.35 - Updated traefik network issues

--- OutsiderProject/outsider-front/src/constants.js
+++ OutsiderProject/outsider-front/src/constants.js
@@ -1,6 +1,6 @@
-// const API_URL = "http://15.188.198.53:8050/";
 const BASE_URL = "";
-const API_URL = "https://api.outsidergame.top";
+// const API_URL = "https://api.outsidergame.top";
+const API_URL = "http://15.188.198.53:8050/";
 const WEBSOCKET_URL = "ws://15.188.198.53:8050/";
 
 const State = {
