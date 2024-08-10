621206d8f94fe015dc77aa3619ed50b1aef5fd5c
Maes95 <michel.maes95@gmail.com>
2024-08-10 17:07:43 +0200

    v1.0.11

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# docker build -t maes95/outsider-backend:1.0.1 .
-# docker push maes95/outsider-backend:1.0.1
-docker build -t maes95/outsider-front:1.0.4 -f outsider-front/Dockerfile outsider-front/
-docker push maes95/outsider-front:1.0.4
+docker build -t maes95/outsider-backend:1.0.2 .
+docker push maes95/outsider-backend:1.0.2
+# docker build -t maes95/outsider-front:1.0.10 -f outsider-front/Dockerfile outsider-front/
+# docker push maes95/outsider-front:1.0.10
--- outsider/asgi.py
+++ outsider/asgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -18,10 +21,6 @@ from channels.security.websocket import AllowedHostsOriginValidator, OriginValid
 from logic.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outsider.settings")
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
